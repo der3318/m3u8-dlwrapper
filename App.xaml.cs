6f70530a11afbc0e68898f1ac2c9d3471aa1c26a
Der Chien <b03902015@ntu.edu.tw>
2022-03-05 21:41:09 +0800

    20220305 init wpf project

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace m3u8_dlwrapper
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
